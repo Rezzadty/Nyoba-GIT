8baaaa90290039c6b8574b4628d730269fe3a41b
Rezzadty <Rezzadty>
2024-07-05 17:52:42 +0700

    Upload Project PBO di Coba GUI

---
+++ src/latihan1/Penulis.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan1;
+
+/**
+ *
+ * @author acer
+ */
+public class Penulis {
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    private int id;
+    private String nama;
+}
